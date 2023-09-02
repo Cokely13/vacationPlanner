138903e7ef98af8c4c69783244f8e5d91c6da84c
Ryan Cokely <ryan.cokely@gmail.com>
2023-09-02 07:40:26 -0400

    trip list updated

--- client/components/TripList.js
+++ client/components/TripList.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import {fetchTrips} from '../store/allTripsStore'
 
 function TripList() {
+  const dispatch = useDispatch()
+  const trips = useSelector((state) => state.allTrips)
+
+  useEffect(() => {
+    dispatch(fetchTrips())
+  }, [])
+
   return (
     <div>
     <div>TripList</div>
-    <div>Location:  / Date: </div>/div>
+    {trips.length? trips.maps((trip) => {
+      return (
+      <div>
+      <div>Name: {trip.name} </div>
+      <div>Location: {trip.location} </div>
+      <div>Length: {trip.length} </div>
+      <div>Response Date: {trip.responseDate} </div>
+      </div>
+    )}) :  <div>Non</div>}
+    <div>Location:  / Date: </div>
     </div>
 
   )
